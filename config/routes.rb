290e4c2acfe19b47075e9ff358c19c395ce5edac
Shawn McSparron <shawnmc_28@hotmail.com>
2022-11-22 20:06:41 +1100

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'tasks#index'
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-  post 'tasks', to: 'tasks#create'
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
-  patch 'tasks/:id', to: 'tasks#update'
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+  resources :tasks
+
 end
